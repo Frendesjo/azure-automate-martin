a0eb0a010f1bee1afa02eb359c12526a83d4b421
frendesjo <martin_larsen90@me.com>
2021-11-11 14:32:15 +0100

    foreach created

--- src/oppgave_3.ps1
+++ src/oppgave_3.ps1
@@ -1,3 +1,23 @@
-$output = $webRequest = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
+$webRequest = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
 
-$output
+$outputJSON = $webRequest.Content
+
+$Output = ConvertFrom-Json -InputObject $outputJSON
+
+
+
+foreach ($card in $Output){
+    Write-Output "$($card.suit[0]),$($card.value)"
+
+}
+
+
+$string = "" 
+
+$string.GetType()
+
+foreach ($card in $Output){
+    $string = $string + "$($card.suit[0])" + "$($card.value)" + ","
+    #exit
+}
+return $string
