8def827c9e6a964b82ee90800c56e16fc5fe12fa
frendesjo <martin_larsen90@me.com>
2021-11-19 13:43:04 +0100

    ny oppgave

---
+++ src/oppgave_6.ps1
@@ -0,0 +1,57 @@
+param (
+    [Parameter ()]
+    [String]
+    $url  = "http://nav-deckofcards.herokuapp.com/shuffle"
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+
+
+$webRequest = Invoke-WebRequest -Uri $url
+
+$outputJSON = $webRequest.Content
+
+$Output = ConvertFrom-Json -InputObject $outputJSON
+
+$string = $null
+#$string.GetType()
+
+foreach ($card in $Output){
+    $string = $string + "$($card.suit[0])" + "$($card.value)" + ","
+}
+#remove last character 
+$string = $string.Substring(0,$string.Length-1)
+
+
+######Oppgave 5
+
+function SumCards {
+
+    #[OutputType([int])]
+
+    $Sum = [int]"0"
+    #$Sum.GetType()
+
+
+    foreach ($card in $Output){
+
+        $CardValue = $card.value
+
+        if (($CardValue -eq "K") -or ($CardValue -eq "Q") -or ($CardValue -eq "J")) {
+            $CardValue = 10
+        }
+        elseif ($CardValue -eq "A") {
+            $CardValue = 11
+        }
+        else {
+        #switch to integer
+        $CardValue = [int]$CardValue
+        }
+
+        $Sum = $Sum + $CardValue
+    }
+    return $Sum
+}
+
+Write-host "Kortstokk:" $string
+Write-Output "Poengsum: $(SumCards)"
